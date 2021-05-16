7c1287dfef83bdbc3097176ce9d31230ec851910
admin <admin@admins-MacBook-Pro.local>
2021-05-17 00:51:35 +0800

    making this project as war

--- src/main/java/com/clock/diameter/DiameterClockApplication.java
+++ src/main/java/com/clock/diameter/DiameterClockApplication.java
@@ -2,11 +2,16 @@ package com.clock.diameter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DiameterClockApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DiameterClockApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiameterClockApplication.class, args);
